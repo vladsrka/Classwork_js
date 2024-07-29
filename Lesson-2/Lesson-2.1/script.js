bfdfa4f679bb6888e4bbefa9110d09663a7a2482
vladsrka <svv.post01@gmail.com>
2024-07-29 21:05:24 +0300

    work

--- Lesson-2/Lesson-2.1/script.js
+++ Lesson-2/Lesson-2.1/script.js
@@ -5,11 +5,18 @@
 // console.log(string);
 // console.log(string2);
 
-const leninSt = ["home 47", "home 32", "offise 22"];
-console.log(leninSt);
+// const leninSt = ["home 47", "home 32", "offise 22"];
+// console.log(leninSt);
 
-leninSt.push("home 11");
-console.log(leninSt);
+// leninSt.push("home 11");
+// console.log(leninSt);
 
-leninSt.pop();
-console.log(leninSt);
+// leninSt.pop();
+// console.log(leninSt);
+
+const array = [1, 2, 3, 4, 5];
+let sum = 0;
+for (let index = 0; index < array.length; index++) {
+  sum += array[index];
+}
+console.log(sum);
