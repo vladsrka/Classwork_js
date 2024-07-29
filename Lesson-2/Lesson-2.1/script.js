85807f800315b83dee5b60126a0a8b26af051650
vladsrka <svv.post01@gmail.com>
2024-07-29 21:07:01 +0300

    work

--- Lesson-2/Lesson-2.1/script.js
+++ Lesson-2/Lesson-2.1/script.js
@@ -1,16 +1,13 @@
 "use strict";
 // const string = "string";
 // const string2 = new String("string 2");
-
 // console.log(string);
 // console.log(string2);
 
 // const leninSt = ["home 47", "home 32", "offise 22"];
 // console.log(leninSt);
-
 // leninSt.push("home 11");
 // console.log(leninSt);
-
 // leninSt.pop();
 // console.log(leninSt);
 
