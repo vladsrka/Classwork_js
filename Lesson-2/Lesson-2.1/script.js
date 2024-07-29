596228eaf2e48943a76c92d89b30944be873c0ab
vladsrka <svv.post01@gmail.com>
2024-07-29 20:47:54 +0300

    work

--- Lesson-2/Lesson-2.1/script.js
+++ Lesson-2/Lesson-2.1/script.js
@@ -1,6 +1,12 @@
 "use strict";
-const string = "string";
-const string2 = new String("string 2");
+// const string = "string";
+// const string2 = new String("string 2");
 
-console.log(string);
-console.log(string2);
+// console.log(string);
+// console.log(string2);
+
+const leninSt = ["home 47", "home 32", "offise 22"];
+console.log(leninSt);
+
+leninSt.push("home 11");
+console.log(leninSt);
